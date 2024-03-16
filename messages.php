32447ff18da0ee56e3e6524f39f94577825c9be4
Fisnik <71096106+Doommafia@users.noreply.github.com>
2024-03-16 21:11:51 +0100

    Create messages.php

---
+++ messages.php
@@ -0,0 +1,38 @@
+<?php
+
+for ($i = 0; $i < 10; $i++) {
+echo'<div class="card">
+        <a class="cardUser" href="#">Jeffrey Jefferson (UID #2940283)</a>
+        <p class="cardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, culpa velit corrupti doloremque maxime odio soluta alias odit ipsam. Natus totam voluptate in architecto deleniti suscipit voluptas libero ad iusto. Laborum at, officia voluptatibus sit rem optio unde laboriosam quo sapiente, error ut repudiandae tempore ratione ullam. Non voluptatem assumenda neque dolor a eaque saepe quam ullam perferendis explicabo dolorum maxime illo, aut perspiciatis doloremque eligendi sapiente placeat. Quaerat nam itaque blanditiis sint quia totam qui commodi nostrum odit, ex temporibus. Consequatur temporibus, voluptatum at distinctio dolorem alias deserunt delectus. Est necessitatibus sint explicabo veniam veritatis esse omnis possimus iste repellendus dolore inventore quaerat maiores accusamus porro, eaque quas rem? Perspiciatis dolorem ab inventore illum quos vitae libero cumque nemo, voluptatum quis. Ullam, amet! Voluptatem praesentium temporibus hic repellendus, esse doloremque asperiores adipisci iste ipsa maiores veniam aspernatur, officiis reprehenderit ipsum incidunt rem impedit harum tempore ad sunt quo numquam.</p>
+    </div>';
+}
+?>
+
+<style>
+    .card {
+    width: 100vh;
+    max-width: 600px; /* Limit card width for readability */
+    background: white;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    position: center;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 20px;
+    margin: 20px;
+    }
+
+    .cardUser {
+        text-decoration: none;
+        font-size: 20px;
+        font-weight: bold;
+        color: #0A0A33;
+    }
+
+    .cardText {
+        color: #000068;
+        font-size: 15px;
+    }
+</style>
