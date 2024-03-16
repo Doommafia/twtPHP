966195ef8bb5825466a744bb9990c529f5a21942
Fisnik <71096106+Doommafia@users.noreply.github.com>
2024-03-16 21:11:28 +0100

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,33 @@
+<script src="https://kit.fontawesome.com/887e44e44e.js" crossorigin="anonymous"></script>   
+
+<div class="sideBar">
+    <div class="sideBar-content">
+        <i class="fa-solid fa-arrow-right fa-2x">   $user</i>
+    </div>
+</div>
+
+<style>
+
+    .sidebar-content{
+        margin-left: 3vh;
+        margin-top: 10px;
+    }
+
+    .sidebar{
+        position: -webkit-sticky;
+        position:sticky;
+        top 0;
+        left: 0;
+        background-color: black;
+        width: 10vh;
+        height: 100dvh;
+        padding: 0;
+        margin: 0;
+        float: left;
+        transition: 0.5s;
+    }
+    .sidebar:hover{
+        transition: 0.5s;
+        width: 30vh;
+    }
+</style>
